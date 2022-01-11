b9206321f908571c8ac8503e6885dfc8d46505c7
Volodymyr Mykhailyk <712680+volodymyr-mykhailyk@users.noreply.github.com>
2022-01-11 14:43:44 +0530

    Add support for database secrets backend

--- lib/settings_reader/vault_resolver/resolver.rb
+++ lib/settings_reader/vault_resolver/resolver.rb
@@ -4,6 +4,7 @@ module SettingsReader
   module VaultResolver
     class Resolver
       IDENTIFIER = 'vault://'.freeze
+      DATABASE_PREFIX = 'database/'.freeze
 
       def resolvable?(value, _path)
         return unless value.respond_to?(:start_with?)
@@ -13,12 +14,23 @@ module SettingsReader
 
       # Expect value in format `vault://mount/path/to/secret?attribute_name`
       def resolve(value, _path)
-        value = value.delete_prefix(IDENTIFIER)
-        mount, secret = value.split('/', 2)
-        secret, attribute = secret.split('?')
-        attribute ||= 'value'
-        secret = ::Vault.kv(mount).read(secret)
+        path = value.delete_prefix(IDENTIFIER)
+        path, attribute = path.split('?')
+        secret = path.start_with?(DATABASE_PREFIX) ? database_secret(path) : kv_secret(path)
         secret && secret.data[attribute.to_sym]
+      end
+
+      #Resolve KV secret
+      def kv_secret(path)
+        mount, path = path.split('/', 2)
+        Vault.kv(mount).read(path)
+      end
+
+      def database_secret(path)
+        Vault.logical.read(path)
+      rescue Vault::HTTPClientError => e
+        raise unless e.message.include?('* unknown role')
+        nil
       end
     end
   end
--- spec/support/helpers/vault.rb
+++ spec/support/helpers/vault.rb
@@ -2,20 +2,17 @@ module Helpers
   module VaultHelpers
     attr_accessor :vault_keys
 
-    def set_vault_value(path, value)
-      mount, secret = path.split('/', 2)
-      secret, attribute = secret.split('?')
+    def set_vault_value(path, data)
       self.vault_keys ||= []
-      self.vault_keys << [mount, secret]
-      serialized_value = value.is_a?(String) ? value : value.to_json
-      Vault.kv(mount).write(secret, attribute => serialized_value)
+      self.vault_keys << path
+      Vault.kv('secret').write(path, data)
     end
 
     def clear_vault_values
       return unless self.vault_keys
 
-      self.vault_keys.each do |mount, secret|
-        Vault.kv(mount).delete(secret)
+      self.vault_keys.each do |path|
+        Vault.kv('secret').delete(path)
       end
       self.vault_keys = []
     end
@@ -27,7 +24,7 @@ RSpec.configure do |config|
 
   config.before(:each, :vault) do
     Vault.address = 'http://127.0.0.1:8200'
-    Vault.token = 'vault_resolver_token'
+    Vault.token = 'vault_root_token'
   end
 
   config.after(:each, :vault) do
