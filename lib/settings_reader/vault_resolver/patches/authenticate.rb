6bc47b996ce6b3f7e60ff321da644b5d34e5de7f
Volodymyr Mykhailyk <712680+volodymyr-mykhailyk@users.noreply.github.com>
2022-03-01 15:32:10 +0200

    Add k8s authentication helpers

---
+++ lib/settings_reader/vault_resolver/patches/authenticate.rb
@@ -0,0 +1,21 @@
+module Vault
+  # Monkey patch to support k8s authenticaiton. Taken from https://github.com/hashicorp/vault-ruby/pull/202
+  class Authenticate < Request
+    def kubernetes(role, route = nil, service_token_path = nil)
+      route ||= '/v1/admin/auth/kubernetes/login'
+      service_token_path ||= '/var/run/secrets/kubernetes.io/serviceaccount/token'
+
+      payload = {
+        role: role,
+        jwt: File.read(service_token_path)
+      }
+
+      json = client.post(route, JSON.fast_generate(payload))
+
+      secret = Secret.decode(json)
+      client.token = secret.auth.client_token
+
+      secret
+    end
+  end
+end
