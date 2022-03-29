0269de80e9a2bdb573b723996abd529e12dc4982
Volodymyr Mykhailyk <712680+volodymyr-mykhailyk@users.noreply.github.com>
2022-03-29 11:59:14 +0300

    Add some tests and ignore spec files in coverage

--- spec/settings_reader/vault_resolver_spec.rb
+++ spec/settings_reader/vault_resolver_spec.rb
@@ -3,13 +3,24 @@ RSpec.describe SettingsReader::VaultResolver do
     expect(SettingsReader::VaultResolver::VERSION).to eq('0.0.0')
   end
 
-  it 'has configured cache' do
-    expect(described_class.cache).to be_instance_of(SettingsReader::VaultResolver::Cache)
+  describe '.cache' do
+    it 'has configured cache' do
+      expect(described_class.cache).to be_instance_of(SettingsReader::VaultResolver::Cache)
+    end
+
+    it 'configures cache only once' do
+      expect(described_class.cache).to eq(described_class.cache)
+    end
   end
 
-  it 'has configured lease refresher' do
-    described_class.refresher_timer_task
-    expect(described_class.refresher_timer_task).to be_instance_of(Concurrent::TimerTask)
+  describe '.refresher_timer_task' do
+    it 'has configured lease refresher' do
+      expect(described_class.refresher_timer_task).to be_instance_of(Concurrent::TimerTask)
+    end
+
+    it 'does not change task on second setup' do
+      expect { described_class.setup_lease_refresher }.not_to change(described_class, :refresher_timer_task)
+    end
   end
 
   it 'returns instance of resolver' do
