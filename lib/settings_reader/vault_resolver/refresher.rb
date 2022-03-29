c06a2d8a9a5dc249e459751f79d4f42853584d5e
Volodymyr Mykhailyk <712680+volodymyr-mykhailyk@users.noreply.github.com>
2022-03-29 16:30:16 +0300

    Refresher to return list of promises for each resolution

--- lib/settings_reader/vault_resolver/refresher.rb
+++ lib/settings_reader/vault_resolver/refresher.rb
@@ -1,3 +1,5 @@
+require 'concurrent/promise'
+
 module SettingsReader
   module VaultResolver
     # Vault Lease refresher task
@@ -13,22 +15,26 @@ module SettingsReader
 
       def refresh
         info { 'Performing Vault leases refresh' }
-        @cache.entries.each do |entry|
+        promises = @cache.entries.map do |entry|
           debug { "Checking lease for #{entry}. Leased?: #{entry.leased?}. Expires in: #{entry.expires_in}s" }
           refresh_entry(entry)
-        end
+        end.compact
+        promises.each(&:wait)
+        promises
       end
 
       def refresh_entry(entry)
         return unless entry.leased?
         return unless entry.expires_in < DEFAULT_RENEW_DELAY
 
-        debug { "Refreshing lease for #{entry}. Expires in: #{entry.expires_in}" }
-        entry.renew
-        info { "Lease renewed for #{entry}. Expires in: #{entry.expires_in}" }
-      rescue StandardError => e
-        error { "Error refreshing lease for #{entry}: #{e.message}" }
-        # Continue renewal.
+        Concurrent::Promise.execute do
+          debug { "Refreshing lease for #{entry}. Expires in: #{entry.expires_in}" }
+          entry.renew
+          info { "Lease renewed for #{entry}. Expires in: #{entry.expires_in}" }
+        rescue StandardError => e
+          error { "Error refreshing lease for #{entry}: #{e.message}" }
+          raise SettingsReader::VaultResolver::Error, e.message
+        end
       end
     end
   end
