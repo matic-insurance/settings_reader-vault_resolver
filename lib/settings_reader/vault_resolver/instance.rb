95ae88859227213cf83f3646b15003d73596efce
Volodymyr Mykhailyk <712680+volodymyr-mykhailyk@users.noreply.github.com>
2022-04-08 15:53:45 +0300

    Use new engines when retreiving secrets

--- lib/settings_reader/vault_resolver/instance.rb
+++ lib/settings_reader/vault_resolver/instance.rb
@@ -13,6 +13,7 @@ module SettingsReader
 
       def initialize(config)
         @config = config
+        @engines = config.vault_engines
       end
 
       def resolvable?(value, _path)
@@ -29,32 +30,12 @@ module SettingsReader
         entry&.value_for(address.attribute)
       end
 
-      # Resolve KV secret
-      def kv_secret(address)
-        debug { "Fetching new kv secret at: #{address}" }
-        Vault.kv(address.mount).read(address.path)
-      rescue Vault::HTTPClientError => e
-        raise SettingsReader::VaultResolver::Error, e.message
-      end
-
-      def database_secret(address)
-        debug { "Fetching new database secret at: #{address}" }
-        Vault.logical.read(address.full_path)
-      rescue Vault::HTTPClientError => e
-        return nil if e.message.include?('* unknown role')
-
-        raise SettingsReader::VaultResolver::Error, e.message
-      end
-
       private
 
       def fetch_entry(address)
         cache.fetch(address) do
           info { "Retrieving new secret at: #{address}" }
-          if (secret = address.mount == DATABASE_MOUNT ? database_secret(address) : kv_secret(address))
-            debug { "Retrieved secret at: #{address}" }
-            SettingsReader::VaultResolver::Entry.new(address, secret)
-          end
+          config.vault_engine_for(address).get(address)
         end
       rescue StandardError => e
         error { "Error retrieving secret: #{address}: #{e.message}" }
