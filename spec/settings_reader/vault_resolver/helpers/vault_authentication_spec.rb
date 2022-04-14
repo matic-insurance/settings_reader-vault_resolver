9a7204fb652a7e7e1afac171a65ce506e2c9aee1
Volodymyr Mykhailyk <712680+volodymyr-mykhailyk@users.noreply.github.com>
2022-04-14 10:29:17 +0300

    Make integration test for authentication

--- spec/settings_reader/vault_resolver/helpers/vault_authentication_spec.rb
+++ spec/settings_reader/vault_resolver/helpers/vault_authentication_spec.rb
@@ -2,29 +2,36 @@ require 'settings_reader/vault_resolver/helpers/vault_authentication'
 
 RSpec.describe SettingsReader::VaultResolver::Helpers::VaultAuthentication do
   let(:helper) { described_class.new }
-  let(:resolver) { SettingsReader::VaultResolver.resolver }
-  let(:dummy_secret) { entry_double }
+  let(:auth_secret) { instance_double(Vault::SecretAuth, client_token: 'vault_client_token', renewable?: true) }
+  let(:secret) { vault_secret_double(auth: auth_secret) }
 
   before do
-    allow(SettingsReader::VaultResolver).to receive(:resolver).and_return(resolver)
-    allow(resolver).to receive(:resolve).and_return(dummy_secret)
+    allow(Vault.auth).to receive(:kubernetes).and_return(secret)
   end
 
   describe '#authenticate_via_k8s' do
     context 'with custom params' do
-      it 'resolves entry' do
+      it 'logins via Vault authentication method' do
         helper.authenticate_via_k8s('test_role', route: 'test_route', service_token_path: '/var/token')
-        params = 'role=test_role&route=test_route&service_token_path=%2Fvar%2Ftoken'
-        url = "vault://auth/kubernetes/login?#{params}#client_token"
-        expect(resolver).to have_received(:resolve).with(url, instance_of(String))
+        params = { route: 'test_route', service_token_path: '/var/token' }
+        expect(Vault.auth).to have_received(:kubernetes).with('test_role', params)
+      end
+
+      it 'returns token' do
+        token = helper.authenticate_via_k8s('test_role', route: 'test_route', service_token_path: '/var/token')
+        expect(token).to eq('vault_client_token')
       end
     end
 
     context 'with minimal params' do
       it 'resolves entry' do
         helper.authenticate_via_k8s('test_role')
-        url = 'vault://auth/kubernetes/login?role=test_role#client_token'
-        expect(resolver).to have_received(:resolve).with(url, instance_of(String))
+        expect(Vault.auth).to have_received(:kubernetes).with('test_role', { route: nil, service_token_path: nil })
+      end
+
+      it 'returns token' do
+        token = helper.authenticate_via_k8s('test_role', route: 'test_route', service_token_path: '/var/token')
+        expect(token).to eq('vault_client_token')
       end
     end
   end
