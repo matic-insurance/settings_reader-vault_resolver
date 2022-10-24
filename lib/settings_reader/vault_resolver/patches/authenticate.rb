f4128a681a9eec1b5428b55b96e79dc945ab11be
DanPolyuha <dpolyuha@gmail.com>
2022-10-24 13:17:36 +0300

    Use VAULT_LOGIN_ENDPOINT environment variable for Authenticate module

--- lib/settings_reader/vault_resolver/patches/authenticate.rb
+++ lib/settings_reader/vault_resolver/patches/authenticate.rb
@@ -2,7 +2,7 @@ module Vault
   # Monkey patch to support k8s authenticaiton. Taken from https://github.com/hashicorp/vault-ruby/pull/202
   class Authenticate < Request
     def kubernetes(role, route: nil, service_token_path: nil)
-      route ||= '/v1/auth/kubernetes/login'
+      route ||= (ENV['VAULT_LOGIN_ENDPOINT'] || '/v1/auth/kubernetes/login')
       service_token_path ||= '/var/run/secrets/kubernetes.io/serviceaccount/token'
 
       payload = {
