bf4a177b5a0bd28390069c5a51c9ee176024c58e
Volodymyr Mykhailyk <712680+volodymyr-mykhailyk@users.noreply.github.com>
2024-01-22 15:31:42 +0200

    Add localstack and AWS secret engine configuration

--- local/vault/app_policy.hcl
+++ local/vault/app_policy.hcl
@@ -1,3 +1,6 @@
+# ------------------------------------------------
+# Static secrets
+# ------------------------------------------------
 # Allow creation of any secrets for test purposes
 path "secret/data/test/*" {
   capabilities = ["create", "read", "update", "delete", "list"]
@@ -24,7 +27,9 @@ path "secret/metadata/deep/*" {
   capabilities = ["list"]
 }
 
-# Access to dynamic credentials
+# ------------------------------------------------
+# Database secrets
+# ------------------------------------------------
 path "database/creds/app-user" {
   capabilities = ["read"]
 }
@@ -36,5 +41,28 @@ path "database/creds/unknown-db" {
 
 #Deep path to allow renewal only for allowed credentials
 path "sys/renew/database/creds/app-user/*" {
+  capabilities = ["create", "update"]
+}
+
+# ------------------------------------------------
+# AWS secrets
+# ------------------------------------------------
+path "aws/creds/app-user" {
+  capabilities = ["read"]
+}
+
+path "aws/creds/app-role" {
+  capabilities = ["read"]
+}
+
+path "aws/static-creds/app-static-user" {
+  capabilities = ["read"]
+}
+
+path "aws/creds/app-missing" {
+  capabilities = ["read"]
+}
+
+path "sys/renew/aws/creds/app-user/*" {
   capabilities = ["create", "update"]
 }
--- local/vault/setup.sh
+++ local/vault/setup.sh
@@ -35,6 +35,35 @@ call_vault POST "v1/secret/data/unreachable/secret" '{"data": {"foo": "a", "bar"
 message "Static secret value"
 call_vault GET "v1/secret/data/pre-configured" ''
 
+message "Setting up aws secret engine"
+call_vault POST "v1/sys/mounts/aws" \
+    '{
+        "type": "aws"
+    }'
+call_vault POST "v1/aws/config/root" \
+    '{
+       "access_key": "root",
+       "secret_key": "secret",
+       "iam_endpoint": "http://aws:4566",
+       "sts_endpoint": "http://aws:4566"
+     }'
+
+call_vault POST "v1/aws/config/lease" '{"lease": "1m", "lease_max": "45m"}'
+call_vault POST "v1/aws/roles/app-user" '{"credential_type": "iam_user", "policy_arns": ["arn:aws:iam::000000000000:policy/app-access"]}'
+call_vault POST "v1/aws/roles/app-role" \
+    '{
+        "credential_type": "assumed_role",
+        "role_arns": ["arn:aws:iam::000000000000:role/app-role"],
+        "default_sts_ttl": "15m",
+        "max_sts_ttl": "45m"
+    }'
+call_vault POST "v1/aws/static-roles/app-static-user" '{"username": "app-static-user", "rotation_period": "5m"}'
+
+message "Verifying aws role"
+call_vault GET "v1/aws/creds/app-user" ''
+call_vault GET "v1/aws/creds/app-role" ''
+call_vault GET "v1/aws/static-creds/app-static-user" ''
+
 message "Setting up dynamic db secret"
 call_vault POST "v1/sys/mounts/database" '{"type": "database"}'
 
@@ -66,7 +95,7 @@ call_vault GET "v1/database/creds/app-user"
 message "Setting up app policy"
 call_vault POST "v1/sys/policies/acl/app" \
     '{
-       "policy": "# Allow creation of any secrets for test purposes\npath \"secret/data/test/*\" {\n  capabilities = [\"create\", \"read\", \"update\", \"delete\", \"list\"]\n}\n\npath \"secret/metadata/test/*\" {\n  capabilities = [\"list\"]\n}\n\n#Access to preconfigured secrets\npath \"secret/data/pre-configured\" {\n  capabilities = [\"read\"]\n}\n\npath \"secret/metadata/pre-configured\" {\n  capabilities = [\"list\"]\n}\n\npath \"secret/data/deep/*\" {\n  capabilities = [\"read\"]\n}\n\npath \"secret/metadata/deep/*\" {\n  capabilities = [\"list\"]\n}\n\n# Access to dynamic credentials\npath \"database/creds/app-user\" {\n  capabilities = [\"read\"]\n}\n\n#allow reading missing creds for tests\npath \"database/creds/unknown-db\" {\n  capabilities = [\"read\"]\n}\n\n#Deep path to allow renewal only for allowed credentials\npath \"sys/renew/database/creds/app-user/*\" {\n  capabilities = [\"create\", \"update\"]\n}\n"
+       "policy": "# ------------------------------------------------\n# Static secrets\n# ------------------------------------------------\n# Allow creation of any secrets for test purposes\npath \"secret/data/test/*\" {\n  capabilities = [\"create\", \"read\", \"update\", \"delete\", \"list\"]\n}\n\npath \"secret/metadata/test/*\" {\n  capabilities = [\"list\"]\n}\n\n#Access to preconfigured secrets\npath \"secret/data/pre-configured\" {\n  capabilities = [\"read\"]\n}\n\npath \"secret/metadata/pre-configured\" {\n  capabilities = [\"list\"]\n}\n\npath \"secret/data/deep/*\" {\n  capabilities = [\"read\"]\n}\n\npath \"secret/metadata/deep/*\" {\n  capabilities = [\"list\"]\n}\n\n# ------------------------------------------------\n# Database secrets\n# ------------------------------------------------\npath \"database/creds/app-user\" {\n  capabilities = [\"read\"]\n}\n\n#allow reading missing creds for tests\npath \"database/creds/unknown-db\" {\n  capabilities = [\"read\"]\n}\n\n#Deep path to allow renewal only for allowed credentials\npath \"sys/renew/database/creds/app-user/*\" {\n  capabilities = [\"create\", \"update\"]\n}\n\n# ------------------------------------------------\n# AWS secrets\n# ------------------------------------------------\npath \"aws/creds/app-user\" {\n  capabilities = [\"read\"]\n}\n\npath \"aws/creds/app-role\" {\n  capabilities = [\"read\"]\n}\n\npath \"aws/static-creds/app-static-user\" {\n  capabilities = [\"read\"]\n}\n\npath \"aws/creds/app-missing\" {\n  capabilities = [\"read\"]\n}\n\npath \"sys/renew/aws/creds/app-user/*\" {\n  capabilities = [\"create\", \"update\"]\n}\n"
      }'
 
 message "Verifying app policy"
