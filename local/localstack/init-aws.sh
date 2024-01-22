7209c417582bb67ca3fc5323085881013635d82c
Volodymyr Mykhailyk <712680+volodymyr-mykhailyk@users.noreply.github.com>
2024-01-22 15:31:42 +0200

    Add localstack and AWS secret engine configuration

---
+++ local/localstack/init-aws.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+awslocal iam create-policy --policy-name app-access --policy-document "$(</etc/localstack/init/ready.d/app-access-policy.json)"
+awslocal iam create-role --role-name app-role --assume-role-policy-document "$(</etc/localstack/init/ready.d/app-assume-policy.json)"
+awslocal iam create-user --user-name app-static-user
+
+awslocal iam attach-role-policy --role-name app-role --policy-arn arn:aws:iam::aws:policy/ReadOnlyAccess
+awslocal iam attach-user-policy --user-name app-static-user --policy-arn arn:aws:iam::aws:policy/ReadOnlyAccess
