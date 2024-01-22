77bca57997b80d9a2a492f43d2c8a5b9e64de721
Volodymyr Mykhailyk <712680+volodymyr-mykhailyk@users.noreply.github.com>
2024-01-22 16:26:54 +0200

    Fail aws init on error

--- local/localstack/init-aws.sh
+++ local/localstack/init-aws.sh
@@ -1,8 +1,14 @@
 #!/bin/bash
 
+set -e
+
+echo "Setting up AWS Stack"
+
 awslocal iam create-policy --policy-name app-access --policy-document "$(</etc/localstack/init/ready.d/app-access-policy.json)"
 awslocal iam create-role --role-name app-role --assume-role-policy-document "$(</etc/localstack/init/ready.d/app-assume-policy.json)"
 awslocal iam create-user --user-name app-static-user
 
 awslocal iam attach-role-policy --role-name app-role --policy-arn arn:aws:iam::aws:policy/ReadOnlyAccess
 awslocal iam attach-user-policy --user-name app-static-user --policy-arn arn:aws:iam::aws:policy/ReadOnlyAccess
+
+echo "Done configuring aws stack"
